55405c4b11e0df0c6217a1278af7fadab2945465
aa <aa@123.com>
2020-04-13 21:36:15 +0800

    拦截器

---
+++ src/main/java/com/wxtest/wechat/service/AccessTokenService.java
@@ -0,0 +1,23 @@
+package com.wxtest.wechat.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ *    
+ * <p>
+ *  Simple to Introduction  
+ *  @ClassName:    AccessTokenService
+ *  @Description: 
+ *  @Author:       87050
+ *  @CreateDate:   2020/4/13
+ *  @UpdateUser:   87050
+ *  @UpdateDate:   2020/4/13
+ *  @UpdateRemark: [初版]
+ *  @Version:      [v1.0] 
+ * <p>
+ *  
+ */
+@Service
+public class AccessTokenService {
+
+}
